20963068d09102a41b7f0f3ce696c799de9c0acf
Iverzhang <iverfighter@gmail.com>
2014-09-07 19:58:25 +0800

    plot4

---
+++ plot4.R
@@ -0,0 +1,45 @@
+#exploratory data Project 1
+#Plot 3
+setwd("F:/Code/R/datacourse/exploratory data")
+data <- read.table("household_power_consumption.txt",
+                   header=TRUE,sep=";",
+                   stringsAsFactors=FALSE)
+data$Date <- as.Date(data$Date,"%d/%m/%y%y")
+subdata <- data[which(data$Date=="2007-02-01"|data$Date=="2007-02-02"),]
+subdata$Global_active_power <- as.numeric(subdata$Global_active_power)
+
+DT <- paste(subdata$Date,subdata$Time)
+subdata$Time <- strptime(DT,"%Y-%m-%d %H:%M:%S")
+opar <- par(no.readonly=TRUE)
+par(mfrow=c(2,2))
+plot(subdata$Time,
+     subdata$Global_active_power,
+     type="l",
+     ylab="Global Active Power(kilowatts)",
+     xlab="")
+plot(subdata$Time,
+     subdata$Voltage,
+     type="l",
+     ylab="Voltage",
+     xlab="datetime")
+plot(subdata$Time,
+     subdata$Sub_metering_1,
+     type="l",
+     ylab="Energy sub metering",
+     xlab="")
+lines(subdata$Time,
+      subdata$Sub_metering_2,
+      col="red")
+lines(subdata$Time,
+      subdata$Sub_metering_3,
+      col="blue")
+legend("topright",
+       c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col=c("black","red","blue"),
+       lty=1,cex=.5)
+plot(subdata$Time,
+     subdata$Global_reactive_power,
+     type="l",
+     ylab="Global_reactive_power",
+     xlab="datetime")
+par(opar)
